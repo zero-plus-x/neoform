5b654b9247930af29f91ba3d24a12f57f9428977
deepsweet <kir@soulshine.in>
2017-01-17 11:54:59 +0100

    :lipstick:

--- demo/simple/index.jsx
+++ demo/simple/index.jsx
@@ -3,75 +3,71 @@ import { render } from 'react-dom';
 import setByPath from 'lodash.set';
 import { Form, Field } from '../../lib/';
 
-const Input = Field(({ changeValue, ...props }) => {
-  return (
-    <input
-      {...props}
-      type="text"
-      onChange={(e) => changeValue(e.target.value)}
-    />
-  );
-});
+const Input = Field(({ changeValue, ...props }) => (
+  <input
+    {...props}
+    type="text"
+    onChange={(e) => changeValue(e.target.value)}
+  />
+));
 
-const MyForm = Form(({ data, addNewFriend }) => {
-  return (
-    <form
-      onSubmit={(e) => {
-        console.log(data);
-        e.preventDefault();
-      }}
-    >
-      <h1>simple form</h1>
-      <h2>personal data</h2>
-      <div>
-        <label>
-          first name
-          <Input name="firstName"/>
-        </label>
-      </div>
-      <div>
-        <label>
-          last name
-          <Input name="lastName"/>
-        </label>
-      </div>
-      <h2>phone numbers</h2>
-      <ul>
-        {
-          data.phoneNumbers.map((phoneNumber, index) => (
-            <li key={index}>
-              <Input name={`phoneNumbers[${index}]`}/>
-            </li>
-          ))
-        }
-      </ul>
-      <h2>friends</h2>
-      <ul>
-        {
-          data.friends.map((friend, index) => (
-            <li key={index}>
-              <div>
-                <label>
-                  first name
-                  <Input name={`friends[${index}].firstName`}/>
-                </label>
-              </div>
-              <div>
-                <label>
-                  last name
-                  <Input name={`friends[${index}].lastName`}/>
-                </label>
-              </div>
-            </li>
-          ))
-        }
-      </ul>
-      <button type="button" onClick={addNewFriend}>add new friend</button>
-      <hr/>
-      <button type="submit">submit</button>
-    </form>
-  );
-});
+const MyForm = Form(({ data, addNewFriend }) => (
+  <form
+    onSubmit={(e) => {
+      console.log(data);
+      e.preventDefault();
+    }}
+  >
+    <h1>simple form</h1>
+    <h2>personal data</h2>
+    <div>
+      <label>
+        first name
+        <Input name="firstName"/>
+      </label>
+    </div>
+    <div>
+      <label>
+        last name
+        <Input name="lastName"/>
+      </label>
+    </div>
+    <h2>phone numbers</h2>
+    <ul>
+      {
+        data.phoneNumbers.map((phoneNumber, index) => (
+          <li key={index}>
+            <Input name={`phoneNumbers[${index}]`}/>
+          </li>
+        ))
+      }
+    </ul>
+    <h2>friends</h2>
+    <ul>
+      {
+        data.friends.map((friend, index) => (
+          <li key={index}>
+            <div>
+              <label>
+                first name
+                <Input name={`friends[${index}].firstName`}/>
+              </label>
+            </div>
+            <div>
+              <label>
+                last name
+                <Input name={`friends[${index}].lastName`}/>
+              </label>
+            </div>
+          </li>
+        ))
+      }
+    </ul>
+    <button type="button" onClick={addNewFriend}>add new friend</button>
+    <hr/>
+    <button type="submit">submit</button>
+  </form>
+));
 
 class App extends Component {
   constructor(props) {
