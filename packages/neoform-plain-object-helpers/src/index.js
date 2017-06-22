b5a5df412f0de6a532d050cbe390b73e341e99a4
deepsweet <kir@belevi.ch>
2017-06-23 00:05:41 +0200

    :wrench: neoform-plain-object-helpers: use `dot-prop-immutable` instead of `lodash.get` and `lodash.set`

--- packages/neoform-plain-object-helpers/src/index.js
+++ packages/neoform-plain-object-helpers/src/index.js
@@ -1,2 +1,9 @@
-export { default as getValue } from 'lodash.get';
-export { default as setValue } from 'lodash.set';
+import dotProp from 'dot-prop-immutable';
+import memoize from 'lodash.memoize';
+
+const makeKeyPath = memoize(
+  (name) => name.replace(/\[(\d+)\]/, '.$1').split('.')
+);
+
+export const getValue = (data, name) => dotProp.get(data, makeKeyPath(name));
+export const setValue = (data, name, value) => dotProp.set(data, makeKeyPath(name), value);
