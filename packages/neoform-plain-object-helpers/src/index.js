cb8fc0a95b790c13fe57bef5e1b3c5714405af3b
Kir Belevich <kir@belevi.ch>
2017-08-20 19:17:45 +0200

    :boom: :wrench: neoform-plain-object-helpers: use dots for arrays in `name` notation

--- packages/neoform-plain-object-helpers/src/index.js
+++ packages/neoform-plain-object-helpers/src/index.js
@@ -1,9 +1,4 @@
 import dotProp from 'dot-prop-immutable';
-import memoize from 'lodash.memoize';
 
-const makeKeyPath = memoize(
-  (name) => name.replace(/\[(\d+)\]/, '.$1').split('.')
-);
-
-export const getValue = (data, name) => dotProp.get(data, makeKeyPath(name));
-export const setValue = (data, name, value) => dotProp.set(data, makeKeyPath(name), value);
+export const getValue = (data, name) => dotProp.get(data, name);
+export const setValue = (data, name, value) => dotProp.set(data, name, value);
