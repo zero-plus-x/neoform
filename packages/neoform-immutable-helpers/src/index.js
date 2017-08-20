0c3d278a455659d687e8a646ca0964bb7c5d2a8a
Kir Belevich <kir@belevi.ch>
2017-08-20 19:16:05 +0200

    :boom: :wrench: neoform-immutable-helpers: use dots for arrays in `name` notation

--- packages/neoform-immutable-helpers/src/index.js
+++ packages/neoform-immutable-helpers/src/index.js
@@ -1,14 +1,2 @@
-import memoize from 'lodash.memoize';
-
-const makeKeyPath = memoize(
-  (name) => name.replace(/\[(\d+)\]/, '.$1').split('.')
-);
-
-export const getValue = (target, name) => target.getIn(
-  makeKeyPath(name)
-);
-
-export const setValue = (target, name, value) => target.setIn(
-  makeKeyPath(name),
-  value
-);
+export const getValue = (target, name) => target.getIn(name.split('.'));
+export const setValue = (target, name, value) => target.setIn(name.split('.'), value);
